ab7a39b4597d338844da87dd74e505114fea6636
Omaremam10 <civil.omaremam@gmail.com>
2025-01-15 14:30:43 -0800

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,11 +1,8 @@
-// Import the functions you need from the SDKs you need
+// Import the necessary Firebase modules
 import { initializeApp } from "firebase/app";
 import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// Your Firebase project configuration
 const firebaseConfig = {
   apiKey: "AIzaSyA5LCQf4Ike69o5lRoVVuhYllV0-FT1YAc",
   authDomain: "shaheenfalcon-b2ea2.firebaseapp.com",
@@ -18,8 +15,7 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app); 
-const db = firebase.firestore();
-const auth = firebase.auth();
+const auth = getAuth(app);
+
 // Export Firebase auth for usage in login.js or other files
 export { auth, signInWithEmailAndPassword };
