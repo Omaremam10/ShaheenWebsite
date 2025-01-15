6b7f40c3335c0aef5bf6dcc2f4ea8a2d2e9efc44
Omaremam10 <civil.omaremam@gmail.com>
2025-01-15 14:43:03 -0800

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,21 +1,18 @@
-// Import the necessary Firebase modules
+// استيراد الدوال اللازمة من SDK Firebase
 import { initializeApp } from "firebase/app";
 import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
 
-// Your Firebase project configuration
+// تكوين Firebase الخاص بك
 const firebaseConfig = {
-  apiKey: "AIzaSyA5LCQf4Ike69o5lRoVVuhYllV0-FT1YAc",
-  authDomain: "shaheenfalcon-b2ea2.firebaseapp.com",
-  projectId: "shaheenfalcon-b2ea2",
-  storageBucket: "shaheenfalcon-b2ea2.firebasestorage.app",
-  messagingSenderId: "823963121655",
-  appId: "1:823963121655:web:cc6d1745a7ac0b517320f2",
-  measurementId: "G-KGEGEPVWHB"
+    apiKey: "AIzaSyA5LCQf4Ike69o5lRoVVuhYllV0-FT1YAc",
+    authDomain: "shaheenfalcon-b2ea2.firebaseapp.com",
+    projectId: "shaheenfalcon-b2ea2",
+    storageBucket: "shaheenfalcon-b2ea2.firebasestorage.app",
+    messagingSenderId: "823963121655",
+    appId: "1:823963121655:web:cc6d1745a7ac0b517320f2",
+    measurementId: "G-KGEGEPVWHB"
 };
 
-// Initialize Firebase
+// تهيئة Firebase
 const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
-
-// Export Firebase auth for usage in login.js or other files
-export { auth, signInWithEmailAndPassword };
+const auth = getAuth(app);  // الحصول على الـ auth من Firebase
